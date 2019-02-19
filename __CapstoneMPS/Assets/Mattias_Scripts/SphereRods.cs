974933d2b411f3afda0a4f6b123f6b35d60fa834
Mattias Lambert <mattiasl380@gmail.com>
2019-02-19 17:16:37 -0500

    Added a new Monolith, Implemented the SphereRodPlate system

---
+++ __CapstoneMPS/Assets/Mattias_Scripts/SphereRods.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class SphereRods : MonoBehaviour
+{
+    public GameObject rod;
+    public float growthSpeed;
+
+    private Vector3[] verts;
+    private Vector3[] normals;
+    private List<GameObject> rods = new List<GameObject>();
+    private List<int> usedIndices = new List<int>();
+
+    public AnimationCurve growthCurve;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        verts = GetComponent<MeshFilter>().mesh.vertices;
+        Debug.Log(verts.Length);
+        normals = GetComponent<MeshFilter>().mesh.normals;
+        Debug.Log(normals.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RodSpawn();
+        }
+    }
+
+    public void RodSpawn()
+    {
+        GameObject newRod = Instantiate(rod);
+        newRod.transform.localScale = new Vector3(1, 0, 1);
+
+        int vertIdx = 0;
+        do
+        {
+            vertIdx = Random.Range(0, verts.Length);
+        } while (usedIndices.Contains(vertIdx));
+        
+        
+        
+        usedIndices.Add(vertIdx);
+        newRod.transform.position = transform.TransformPoint(verts[vertIdx]);
+        newRod.transform.up = transform.TransformVector(normals[vertIdx]);
+
+        StartCoroutine(RodGrow(newRod));
+    }
+
+    
+    public IEnumerator RodGrow(GameObject growingRod)
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            growingRod.transform.localScale = new Vector3(0.5f, Mathf.LerpUnclamped(0, 1, growthCurve.Evaluate(t)), 0.5f);
+            t += Time.deltaTime * growthSpeed;
+            yield return 0;
+        }
+    }
+    
+}
