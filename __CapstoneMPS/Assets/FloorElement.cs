537d28ef3fb18303108da4a9623b9983e82de3c1
Patrick Perkins <plp248@nyu.edu>
2019-03-10 16:18:23 -0400

    first floor of totem 1 and sound chip

---
+++ __CapstoneMPS/Assets/FloorElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorElement : MonoBehaviour
+{
+    public AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+        
+        source.Play();
+        Debug.Log("stepped");
+    }
+}
---
+++ __CapstoneMPS/Assets/Scripts/Patrick_Scripts/MusicChip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChip : MonoBehaviour
+{
+
+    public AudioClip support;
+    public AudioClip[] elements;
+    
+}
