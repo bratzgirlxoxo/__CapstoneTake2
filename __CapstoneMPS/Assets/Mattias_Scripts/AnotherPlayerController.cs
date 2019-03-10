1e8b11003c09e9b5ffb4109a99f93a86e2856688
Mattias Lambert <mattiasl380@gmail.com>
2019-03-10 16:31:52 -0400

    Added Concrete Material for testing

--- __CapstoneMPS/Assets/Mattias_Scripts/AnotherPlayerController.cs
+++ __CapstoneMPS/Assets/Mattias_Scripts/AnotherPlayerController.cs
@@ -26,18 +26,31 @@ public class AnotherPlayerController : MonoBehaviour
                 // We are grounded, so recalculate
                 // move direction directly from axes
     
+                /*
                 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
                 moveDirection = transform.TransformDirection(moveDirection);
                 moveDirection = moveDirection * speed;
+                */
     
+                /*
                 if (Input.GetButton("Jump"))
                 {
                     moveDirection.y = jumpSpeed;
                 }
+                */
+            }
+            
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+            
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
             }
     
             // Apply gravity
-            moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+            moveDirection.y = moveDirection.y - (gravity * 5f * Time.deltaTime);
     
             // Move the controller
             controller.Move(moveDirection * Time.deltaTime);
---
+++ __CapstoneMPS/Assets/Mattias_Scripts/SetCameraDepthTextureMode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraDepthTextureMode : MonoBehaviour
+{
+
+    [SerializeField] private DepthTextureMode depthTextureMode;
+
+    void OnValidate()
+    {
+        SetCamDepthTextureMode();
+    }
+
+    void Awake()
+    {
+        SetCamDepthTextureMode();
+    }
+
+    void SetCamDepthTextureMode()
+    {
+        GetComponent<Camera>().depthTextureMode = depthTextureMode;
+    }
+}
